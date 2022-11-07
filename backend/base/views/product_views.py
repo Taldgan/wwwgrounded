a2147aa3b5dc05529fe3bc8e2e893c8ac0e5af5d
Taldgan <bananaphone993@live.com>
2022-11-07 02:50:04 -0600

    separation of product/user/order views/urls

---
+++ backend/base/views/product_views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product
+from base.serializers import ProductSerializer
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework import status
+
+# Product views
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
