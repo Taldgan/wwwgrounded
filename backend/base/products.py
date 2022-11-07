6fbdac558195bbb3f9189e1f21d6ce52b2c5e787
Taldgan <bananaphone993@live.com>
2022-11-06 20:17:40 -0600

    base of backend api started, base of database models created

---
+++ backend/base/products.py
@@ -0,0 +1,27 @@
+products = [
+        {
+            '_id': '1',
+            'name': 'Airpods Wireless Bluetooth Headphones',
+            'image': '/images/airpods.jpg',
+            'description': 'Bluetooth!',
+            'brand': 'Apple',
+            'category': 'Electronics',
+            'price': 89.99,
+            'countInStock': 10,
+            'rating': 4.5,
+            'numReviews': 12,
+        },
+        {
+            '_id': '2',
+            'name': 'Echo Dot',
+            'image': '/images/echo.jpg',
+            'description': 'Scary',
+            'brand': 'Amazon',
+            'category': 'Electronics',
+            'price': 20.99,
+            'countInStock': 2,
+            'rating': 2.5,
+            'numReviews': 2,
+        },
+]
+
