00add78c4cea2b012aa097f8816b4c3db5bc14ee
Taldgan <bananaphone993@live.com>
2022-11-07 03:35:36 -0600

    add orders, get orders, pay orders

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -64,7 +64,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_shippingAddress(self, obj):
         try:
-            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+            address = ShippingAddressSerializer(obj.shippingaddress, many=False).data
         except:
             address = False
         return address
@@ -73,3 +73,4 @@ class OrderSerializer(serializers.ModelSerializer):
         user = obj.user
         serializer = UserSerializer(user, many=False)
         return serializer.data
+
