cffe2a2d0f188347f574d77c6b9af8d20758d9bd
Taldgan <bananaphone993@live.com>
2022-11-08 16:52:21 -0600

    frontend beginning

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+	<Header />
+	My App 
+	<Footer />
+    </div>
+  );
+}
+
+export default App;
