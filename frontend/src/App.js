62ec2c440ed8b5f452da0f1851235d2fdd43ea94
Taldgan <bananaphone993@live.com>
2022-11-09 03:17:47 -0600

    user login/logout & navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/login' element={<LoginScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
-            <Route path='/cart/' element={<CartScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
           </Routes>
         </Container>
